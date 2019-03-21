02c9add1773eceba5128f4a92c851413253bd583
Marian Hello <m.hello@gmail.com>
2019-03-21 13:45:31 +0100

    reproduce for nestjs 6.0.2

--- src/middlewares/logger-all.middleware.ts
+++ src/middlewares/logger-all.middleware.ts
@@ -1,12 +1,10 @@
-import { Injectable, NestMiddleware, MiddlewareFunction } from '@nestjs/common';
-import express = require('express');
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class AllLoggerMiddleware implements NestMiddleware {
-  resolve(): MiddlewareFunction<express.Request, express.Response> {
-    return (req, res, next) => {
-      console.log(`AllLoggerMiddleware ${req.method} ${req.path}`);
-      next();
-    };
+  use(req: Request, res: Response, next: Function) {
+    console.log(`AllLoggerMiddleware ${req.method} ${req.path}`);
+    next();
   }
 }
